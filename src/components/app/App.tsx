f28f74d8c778f2f3690d718d13be2159c796fec6
Rostyslav <rostikbovonenko@gmail.com>
2023-08-04 13:36:47 +0200

    added routing

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,21 +1,49 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 import Header from "../header/Header";
+
 import SchedulePage from "../../pages/schedule/SchedulePage";
-// import HistoryPage from "../../pages/history/HistoryPage";
-// import CancelModal from "../modal/CancelModal";
+import HistoryPage from "../../pages/history/HistoryPage";
+import PageNotFound from "../../pages/404/404";
+
 import AppointmentsContextProvider from "../../context/appointments/AppointmentsContext";
 
 import "./app.scss";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+		errorElement: <PageNotFound />,
+		children: [
+			{
+				path: "/",
+				element: <SchedulePage />,
+			},
+			{
+				path: "/schedule",
+				element: <SchedulePage />,
+			},
+			{
+				path: "/history",
+				element: <HistoryPage />,
+			},
+		],
+	},
+]);
+
 function App() {
+	return <RouterProvider router={router} />;
+}
+
+function Root() {
 	return (
-		<AppointmentsContextProvider>
-			<main className="board">
-				<Header />
-				<SchedulePage />
-				{/* <HistoryPage /> */}
-				{/* <CancelModal /> */}
-			</main>
-		</AppointmentsContextProvider>
+		<main className="board">
+			<Header />
+			<AppointmentsContextProvider>
+				<Outlet />
+			</AppointmentsContextProvider>
+		</main>
 	);
 }
 
